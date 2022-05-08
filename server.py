21b0ce09690816e3b7910545e5cbfbe9143605a4
Jared Mimms <layermims@gmail.com>
2022-05-08 11:54:17 -0700

    Homeless Clearinghouse

--- server.py
+++ server.py
@@ -17,7 +17,7 @@ app.config["SESSION_TYPE"] = "filesystem"
 app.config.update(SESSION_COOKIE_SAMESITE="None", SESSION_COOKIE_SECURE=True)
 
 Session(app)
-CORS(app, supports_credentials=True,origin='http://www.homelessclearinghouse.com',headers=['Content-Type','Authorization'])
+CORS(app, supports_credentials=True,origins=['http://www.homelessclearinghouse.com'],allow_headers='*')
 
 # Configure CS50 Library to use SQLite database
 db = SQL("sqlite:///offers.db")
@@ -29,7 +29,7 @@ def after_request(response):
     response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
     response.headers["Expires"] = 0
     response.headers["Pragma"] = "no-cache"
-    response.headers["Access-Control-Allow-Origin"] = '*'
+    response.headers["Access-Control-Allow-Origin"] = 'http://www.homelessclearinghouse.com'
     response.headers['Access-Control-Allow-Headers'] = 'Content-Type,Authorization'
     response.headers['Access-Control-Allow-Methods'] = 'POST,GET,PUT,DELETE'
     response.headers['Access-Control-Allow-Credentials'] = 'true'
