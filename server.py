b003dc20ab7713798ffbb2606ace34dc030c370e
Jared Mimms <layermims@gmail.com>
2022-05-08 11:55:45 -0700

    Homeless Clearinghouse

--- server.py
+++ server.py
@@ -29,10 +29,10 @@ def after_request(response):
     response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
     response.headers["Expires"] = 0
     response.headers["Pragma"] = "no-cache"
-    response.headers["Access-Control-Allow-Origin"] = 'http://www.homelessclearinghouse.com'
-    response.headers['Access-Control-Allow-Headers'] = 'Content-Type,Authorization'
-    response.headers['Access-Control-Allow-Methods'] = 'POST,GET,PUT,DELETE'
-    response.headers['Access-Control-Allow-Credentials'] = 'true'
+    # response.headers["Access-Control-Allow-Origin"] = 'http://www.homelessclearinghouse.com'
+    # response.headers['Access-Control-Allow-Headers'] = 'Content-Type,Authorization'
+    # response.headers['Access-Control-Allow-Methods'] = 'POST,GET,PUT,DELETE'
+    # response.headers['Access-Control-Allow-Credentials'] = 'true'
     return response
 
 @app.route("/auth", methods=["POST"])
