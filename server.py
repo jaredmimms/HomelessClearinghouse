359db05a6139f8f547ec3005aad05e9873ccaaa7
Jared Mimms <layermims@gmail.com>
2022-05-08 11:50:36 -0700

    Homeless Clearinghouse

--- server.py
+++ server.py
@@ -29,7 +29,7 @@ def after_request(response):
     response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
     response.headers["Expires"] = 0
     response.headers["Pragma"] = "no-cache"
-    response.headers["Access-Control-Allow-Origin"] = 'http://www.homelessclearinghouse.com'
+    response.headers["Access-Control-Allow-Origin"] = '*'
     response.headers['Access-Control-Allow-Headers'] = 'Content-Type,Authorization'
     response.headers['Access-Control-Allow-Methods'] = 'POST,GET,PUT,DELETE'
     response.headers['Access-Control-Allow-Credentials'] = 'true'
